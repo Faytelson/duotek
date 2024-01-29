160fc055a79419786846ab2b97d88463ef7ee17a
Faytelson <dfaitelson@scid.ru>
2024-01-29 13:46:55 +0300

    mode history

--- src/main.js
+++ src/main.js
@@ -3,16 +3,17 @@ import VueRouter from "vue-router";
 import App from "./App.vue";
 import { routes } from "./routes.js";
 import { store } from "./store/store";
-import { BootstrapVue } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
 const router = new VueRouter({
   routes,
+  mode: "history",
 });
 
 new Vue({
